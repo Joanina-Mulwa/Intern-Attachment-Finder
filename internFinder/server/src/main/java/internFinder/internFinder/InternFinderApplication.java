7dd21b70063f258e22f685b33006159e51bafb93
Joanina_Mulwa <joaninahmulwa@gmail.com>
2022-07-05 09:50:07 +0300

    Initial Commit

---
+++ internFinder/server/src/main/java/internFinder/internFinder/InternFinderApplication.java
@@ -0,0 +1,13 @@
+package internFinder.internFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternFinderApplication.class, args);
+	}
+
+}
---
+++ internFinder/server/src/test/java/internFinder/internFinder/InternFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package internFinder.internFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
