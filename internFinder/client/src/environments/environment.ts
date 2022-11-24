064dae4d62c965d98d9d821a6ec5a242b745c8ca
Joanina_Mulwa <joaninahmulwa@gmail.com>
2022-11-24 22:04:12 +0300

    Changed environment variables

--- internFinder/client/src/environments/environment.ts
+++ internFinder/client/src/environments/environment.ts
@@ -8,20 +8,20 @@ export const environment = {
 
 
 };
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const DB_HOST = process.env["DB_HOST"];
-const DB_USER = process.env["DB_USER"];
-const DB_PASS = process.env["DB_PASS"];
-const DB_NAME = process.env["DB_NAME"];
+// const DB_HOST = process.env["DB_HOST"];
+// const DB_USER = process.env["DB_USER"];
+// const DB_PASS = process.env["DB_PASS"];
+// const DB_NAME = process.env["DB_NAME"];
 
-const connection = mysql.createConnection({
-  host: `/cloudsql/${process.env["DB_INSTANCE"]}`,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
-  socketPath: `/cloudsql/${process.env["DB_INSTANCE"]}`,
-});
+// const connection = mysql.createConnection({
+//   host: `/cloudsql/${process.env["DB_INSTANCE"]}`,
+//   user: DB_USER,
+//   password: DB_PASS,
+//   database: DB_NAME,
+//   socketPath: `/cloudsql/${process.env["DB_INSTANCE"]}`,
+// });
 
 
 /*
